b3d50778c0e8b225cb74af3ea0958631e0ad3e60
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 11:17:33 -0300

    fix: adicionando ajustes

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,12 +1,14 @@
 terraform {
   backend "s3" {
-    bucket   = "meu-terraform-state-hauwei-wave"
+    bucket   = "meu-terraform-state-hauwei-wave" 
     key      = "terraform.tfstate"
     region   = "la-south-2"
     endpoint = "https://obs.la-south-2.myhuaweicloud.com"
-    
+
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true 
+    skip_s3_checksum            = true 
   }
 }
