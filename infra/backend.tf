fd1d69ee92676f8582e6b152c784dbabcd057fae
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 11:26:50 -0300

    fix: forçando path_style e desabilitando s3_checksum

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,15 +1,22 @@
 terraform {
   backend "s3" {
-    bucket   = "meu-terraform-state-hauwei-wave" 
+    bucket   = "seu-nome-de-bucket-aqui" # Use o nome que ficou verde
     key      = "terraform.tfstate"
     region   = "la-south-2"
+    
+    # Sintaxe atualizada para evitar o Warning
     endpoints = {
       s3 = "https://obs.la-south-2.myhuaweicloud.com"
     }
+
+    # Configurações de compatibilidade total com Huawei OBS
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
-    skip_s3_checksum            = true 
+    
+    # ESTAS DUAS LINHAS SÃO A SOLUÇÃO PARA O SEU ERRO
+    use_path_style              = true 
+    skip_s3_checksum            = true
   }
 }
