dcc7123ab693507a50914aca0a5e7d69ae9dd474
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 12:41:59 -0300

    fix: removendo argumento unsupported e usando AWS_EC2_METADATA_DISABLED

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    bucket   = "meu-terraform-state-hauwei-wave"
+    bucket   = "meu-terraform-state-hauwei-wave" # Certifique-se da escrita exata
     key      = "terraform.tfstate"
     region   = "la-south-2"
     endpoint = "https://obs.la-south-2.myhuaweicloud.com"
 
-    # Configurações recomendadas pelo guia do provider
+    # Flags que são 100% suportadas na versão 1.5.7
     skip_region_validation      = true
-    skip_metadata_api_check     = true
     skip_credentials_validation = true
+    skip_metadata_api_check     = true
     
-    # Necessário para evitar que o Terraform busque serviços da AWS
-    skip_requesting_account_id  = true
+    # Resolve o erro de integridade de dados na Huawei Cloud
+    skip_s3_checksum            = true
   }
 }
