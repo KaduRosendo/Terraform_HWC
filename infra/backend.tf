bb88db8fe497a76e59c9455be5542e2dd6686046
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 12:31:03 -0300

    feat: ativando backend remoto agora que o bucket existe

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,19 +1,18 @@
 terraform {
-# COMENTE TUDO ISSO PARA O PRÓXIMO PUSH
-#  backend "s3" {
-#    bucket   = "meu-terraform-state-hauwei-wave"
-#    key      = "terraform.tfstate"
-#    region   = "la-south-2"
-#    
-#    # Sintaxe nova que remove o Warning:
-#    endpoints = {
-#      s3 = "https://obs.la-south-2.myhuaweicloud.com"
-#    }
-#
-#    skip_region_validation      = true
-#    skip_credentials_validation = true
-#    skip_metadata_api_check     = true
-#    skip_requesting_account_id  = true
-#    skip_s3_checksum            = true
-#  }
+  backend "s3" {
+    bucket   = "meu-terraform-state-hauwei-wave"
+    key      = "terraform.tfstate"
+    region   = "la-south-2"
+    
+    # Sintaxe nova que remove o Warning:
+    endpoints = {
+      s3 = "https://obs.la-south-2.myhuaweicloud.com"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
