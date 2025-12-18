f6493ecde60bf5fb8fc211ddff81154c4993bd7f
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 12:25:40 -0300

    fix: configuração definitiva de backend para Huawei OBS

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,20 +1,20 @@
 terraform {
   backend "s3" {
-    bucket   = "meu-terraform-state-hauwei-wave" # Certifique-se de que o nome está identico ao console
+    bucket   = "meu-terraform-state-hauwei-wave"
     key      = "terraform.tfstate"
     region   = "la-south-2"
     endpoint = "https://obs.la-south-2.myhuaweicloud.com"
 
-    # Desativa todas as verificações específicas da AWS
+    # Configurações obrigatórias para Huawei Cloud
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
     
-    # O SEGREDO: Força o Terraform a ignorar o Checksum que está dando erro
+    # Esta linha desativa o cabeçalho que causa o erro de Mismatch
     skip_s3_checksum            = true
-    
-    # IMPORTANTE: Mantenha como false (ou comente) para evitar o erro de Virtual Host
-    # use_path_style            = true 
+
+    # Mantendo como false para usar Virtual Host Style
+    use_path_style              = false
   }
 }
