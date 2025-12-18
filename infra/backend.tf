e57e1c660c111e832987ba9809501ee2440d25b7
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 11:14:53 -0300

    fix: chore: ativando backend remote no OBS

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,11 +1,12 @@
-# terraform {
-#   backend "s3" {
-#     bucket   = "BackendOBSWave" 
-#     key      = "terraform.tfstate"
-#     region   = "la-south-2a"
-#     endpoint = "https://obs.la-south-2a.myhuaweicloud.com"
-#     skip_region_validation      = true
-#     skip_credentials_validation = true
-#     skip_metadata_api_check     = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket   = "meu-terraform-state-hauwei-wave"
+    key      = "terraform.tfstate"
+    region   = "la-south-2"
+    endpoint = "https://obs.la-south-2.myhuaweicloud.com"
+    
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
