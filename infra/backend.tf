aee74dad02c35fc5738fd1e68ca7db19b6370c38
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 11:32:37 -0300

    fix: removendo path_style para resolver VirtualHostDomainRequired

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,22 +1,23 @@
 terraform {
   backend "s3" {
-    bucket   = "meu-terraform-state-hauwei-wave"
+    bucket   = "meu-terraform-state-hauwei-wave" # Verifique se a escrita está correta no console
     key      = "terraform.tfstate"
     region   = "la-south-2"
     
-    # Sintaxe atualizada para evitar o Warning
     endpoints = {
       s3 = "https://obs.la-south-2.myhuaweicloud.com"
     }
 
-    # Configurações de compatibilidade total com Huawei OBS
+    # Configurações essenciais para Huawei OBS
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
+    skip_requesting_account_id  = true # Necessário para não buscar conta AWS
     
-    # ESTAS DUAS LINHAS SÃO A SOLUÇÃO PARA O SEU ERRO
-    use_path_style              = true 
-    skip_s3_checksum            = true
+    # Mantenha esta linha para evitar o erro anterior de SHA256
+    skip_s3_checksum            = true 
+    
+    # REMOVA OU COMENTE A LINHA ABAIXO:
+    # use_path_style           = true 
   }
 }
