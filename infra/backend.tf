2059ea6fd1914e7445f2522ddbe713c1b5def391
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 12:34:51 -0300

    feat: mudando a versão do terraform

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,16 +3,14 @@ terraform {
     bucket   = "meu-terraform-state-hauwei-wave"
     key      = "terraform.tfstate"
     region   = "la-south-2"
-    
-    # Sintaxe nova que remove o Warning:
-    endpoints = {
-      s3 = "https://obs.la-south-2.myhuaweicloud.com"
-    }
+    endpoint = "https://obs.la-south-2.myhuaweicloud.com"
 
+    # Configurações recomendadas pelo guia do provider
     skip_region_validation      = true
-    skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_credentials_validation = true
+    
+    # Necessário para evitar que o Terraform busque serviços da AWS
     skip_requesting_account_id  = true
-    skip_s3_checksum            = true
   }
 }
