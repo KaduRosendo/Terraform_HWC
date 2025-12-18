a0d20f0a9a18d40fa3424ffedb10422283a43b2d
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 13:23:35 -0300

    feat: adicionando workflow de destruição segura

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,16 +1,13 @@
 terraform {
   backend "s3" {
-    bucket   = "meu-terraform-state-hauwei-wave" # Certifique-se da escrita exata
+    bucket   = "meu-terraform-state-hauwei-wave" 
     key      = "terraform.tfstate"
     region   = "la-south-2"
     endpoint = "https://obs.la-south-2.myhuaweicloud.com"
 
-    # Flags que são 100% suportadas na versão 1.5.7
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     
-    # Resolve o erro de integridade de dados na Huawei Cloud
-    # skip_s3_checksum            = true
   }
 }
