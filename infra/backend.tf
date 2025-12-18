9fadea73743bcb0b0f6bc171ddce08041e93653f
Carlos Eduardo Rosendo Basseto <c50047781@china.huawei.com>
2025-12-18 12:28:06 -0300

    chore: desativando backend para recriar bucket

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,20 +1,19 @@
 terraform {
-  backend "s3" {
-    bucket   = "meu-terraform-state-hauwei-wave"
-    key      = "terraform.tfstate"
-    region   = "la-south-2"
-    endpoint = "https://obs.la-south-2.myhuaweicloud.com"
-
-    # Configurações obrigatórias para Huawei Cloud
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    
-    # Esta linha desativa o cabeçalho que causa o erro de Mismatch
-    skip_s3_checksum            = true
-
-    # Mantendo como false para usar Virtual Host Style
-    use_path_style              = false
-  }
+# COMENTE TUDO ISSO PARA O PRÓXIMO PUSH
+#  backend "s3" {
+#    bucket   = "meu-terraform-state-hauwei-wave"
+#    key      = "terraform.tfstate"
+#    region   = "la-south-2"
+#    
+#    # Sintaxe nova que remove o Warning:
+#    endpoints = {
+#      s3 = "https://obs.la-south-2.myhuaweicloud.com"
+#    }
+#
+#    skip_region_validation      = true
+#    skip_credentials_validation = true
+#    skip_metadata_api_check     = true
+#    skip_requesting_account_id  = true
+#    skip_s3_checksum            = true
+#  }
 }
